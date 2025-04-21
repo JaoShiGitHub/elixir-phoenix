4e74e2985c17d8b7281eb7b16565ecc90c09d2b0
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-21 09:38:06 +0700

    Create user controller

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,13 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def user(conn, _params) do
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Ben", email: "ben@email.com"}
+    ]
+    # render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+  end
 end
