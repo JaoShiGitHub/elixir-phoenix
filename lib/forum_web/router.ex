3db373fe248b43eb207a2bce2f878429b090713f
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-04-21 09:36:53 +0700

    Create user route

--- lib/forum_web/router.ex
+++ lib/forum_web/router.ex
@@ -18,6 +18,7 @@ defmodule ForumWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/users", PageController, :user
   end
 
   # Other scopes may use custom stacks.
